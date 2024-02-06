a867e5946db7bd175491a1022a5395da324b4f13
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-07 00:51:22 +0530

    added

---
+++ src/models/user.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      required: [true, "Please Enter ID"],
+    },
+    name: {
+      type: String,
+      required: [true, "Please enter Name"],
+    },
+    email: {
+      type: String,
+      unique: [true, "Email already Exist"],
+      required: [true, "Please enter Email"],
+      validate:
+    },
+    photo: {
+      type: String,
+      required: [true, "Please add Photo"],
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female"],
+      required: [true, "Please enter Gender"],
+    },
+    dob: {
+      type: Date,
+      required: [true, "Please enter DOB"],
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", schema);
