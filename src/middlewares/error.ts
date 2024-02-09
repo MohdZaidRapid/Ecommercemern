2b839ea82998aeccc1ac41311f5103432d213ad0
MohdZaidRapid <www.mohdzaid2902@gmail.com>
2024-02-09 23:00:03 +0530

    added

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/utility-class.js";
+import { ControllerType } from "../types/types.js";
 
 export const errorMiddleware = (
   err: ErrorHandler,
@@ -15,6 +16,10 @@ export const errorMiddleware = (
   });
 };
 
-export const TryCatch = () => () => {
-    
+export const TryCatch = (func: ControllerType) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    return Promise.resolve(func(req, res, next)).catch(next);
+  };
 };
+
+// const a = TryCatch();
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { newUser } from "../controllers/user.js";
+import { newUser, getAllUsers, getUser } from "../controllers/user.js";
 
 const app = express.Router();
 
 // route - /api/v1/user/new
 app.post("/new", newUser);
+
+// route - /api/v1/user/all
+app.get("/all", getAllUsers);
+
+// route - /api/v1/user/dynamicID
+app.get("/:id", getUser);
 
 export default app;
